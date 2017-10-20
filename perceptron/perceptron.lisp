e4c542370e1bf2a36cd730318db93742a36cab8d
José Silva <jcnascimento1994@gmail.com>
2017-10-20 15:41:24 -0300

    Adicionado io-handler + perceptron struct

--- perceptron/perceptron.lisp
+++ perceptron/perceptron.lisp
@@ -1,39 +1,46 @@
+(defstruct perceptron
+    weights
+    input-num
+    signum-function)
+
+
+;; Função que retorna a saída do perceptron dado certas entradas
+;;     Parâmetros:
+;;         p: a estrutura do perceptron.
+;;         i: as entradas atuais.
+;;     Retorna:
+;;         O valor de saída do perceptron para as entradas atuais.
+(defun perceptron (p i)
+    (let ((linear-comb))
+        (setf linear-comb (linear-combiner i (perceptron-weights p)))
+        (funcall (eval (perceptron-signum-function p)) linear-comb)))
+
 ;; Dado os parâmetros o algoritmo executa o "Perceptron Convergence Algorithm" e retorna uma lista com os pesos
-;; na ordem das suas respectivas entradas, conforme passado como parâmetro. Vários comandos para imprimir textos no terminal,
-;; para motivos de acompanhamento do algoritmo, foram inseridos ao longo da função, para ativá-los basta 'descomentar'.
+;; na ordem das suas respectivas entradas, conforme passado como parâmetro.
 ;;     Parâmetros
-;;         -inputs: uma lista onde o tamanho define o número de entradas, e cada elemento contém uma lista de valores
-;;         possíveis a serem tomados pela entrada correspondente àquele índice. Caso deseje usar o bias, coloque-o como uma
-;;         entrada na lista que contém somente o 1, o que indica uma entrada constante.
+;;         -input-num: número de entradas.
+;;         -inputs: A lista onde cada elemento é uma lista de valores de entradas.
 ;;         - desired-responses: uma lista onde cada elemento é uma combinação de entradas onde a saída desejada é 1. todas
 ;;         as outras combinações de entradas não pertencentes à esta lista terão como saída default-response.
 ;;             Optativos:
-;;         - weights: uma lista contendo os pesos iniciais do algoritmo. Inicialmente todos os pesos são setados em 1 por 
-;;         padrão.
 ;;         - learning-rate: parâmetro que define a taxa de aprendizado, deve ser na faixa 0 <= learning-rate <= 1.
-;;         - signum-function: a função limitadora de saídas. Deve receber como parâmetro uma única variável v. Por padrão é
-;;                 +1 -> v > 0
-;;                 -1 -> v <= 0
-;;             Onde v é a combinação linear das entradas*pesos.
 ;;         - max-n: o número de repetições máxima para o algoritmo, caso o número de repetições exceda o máximo, a função
 ;;         retorna nil. Isto serve para impedir loops infinitos em problemas cujas saídas desejáveis não são linearmente
 ;;         separáveis. Por padrão são 1000 repetições máximas
 ;;     Retorna:
 ;;         Uma lista contendo os pesos para a solução do problema, na ordem de suas entradas correspondentes no parâmetro input.
-;;         O peso do bias é retornado como a primeira entrada, seguida pelos outros pesos.
 (defun perceptron-conv (input-num inputs desired-responses &optional &key (learning-rate 0.1) (signum-function nil) (max-n 1000))
-    (let ((weights) (s-func) (n))
+    (let ((weights) (n))
         (setf weights (make-list input-num :initial-element 1))
-        (if (null signum-function)
-            (setf s-func (lambda (v) (if (<= v 0) -1 1)))
-            (setf s-func signum-function))
+        (when (not signum-function)
+            (setf signum-function (lambda (v) (if (<= v 0) -1 1))))
         (dotimes (n max-n) 
             (let ((has-error))
                 (setf has-error nil)
                 (loop for current-input in inputs and desired-response in desired-responses do
                     (let ((va) (err) (ya))
                         (setf va (linear-combiner current-input weights))
-                        (setf ya (funcall s-func va))
+                        (setf ya (funcall signum-function va))
                         (if (not (= ya desired-response))
                             (progn
                                 (setf has-error t)
